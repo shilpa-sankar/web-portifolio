9977991a5aa1f6b8649f8bf11df4584ecd067de7
shilpa <shilpa@thesolarlabs.com>
2023-08-21 17:35:57 +0530

    first commit

---
+++ my-vite-threejs-app/main.js
@@ -0,0 +1,51 @@
+import '../my-vite-threejs-app/style.css';
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+console.log('renderer: ', renderer);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ( scene, camera);
+
+const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    console.log('cube: ', cube);
+    scene.add(cube);
+    console.log('scene: ', scene);
+    
+    // Position the camera
+    camera.position.z = 5;
+    
+    // Create an animation loop
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene,camera);
+    } 
+    animate();
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
